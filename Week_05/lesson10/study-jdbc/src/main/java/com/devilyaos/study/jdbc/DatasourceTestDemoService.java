5bd0cd6d8b8a33dfb58dd39fe91348ba1bb5bb9f
姚舜 <devilyaos@gmail.com>
2021-02-20 00:08:44 +0800

    lession增加hikari连接池的demo

--- Week_05/lesson10/study-jdbc/src/main/java/com/devilyaos/study/jdbc/DatasourceTestDemoService.java
+++ Week_05/lesson10/study-jdbc/src/main/java/com/devilyaos/study/jdbc/DatasourceTestDemoService.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 /**
  * 纯jdbc操作
  */
-public class OnlyJdbcService {
+public class DatasourceTestDemoService {
 
     public static void main(String[] args) {
         // 连接数据库
